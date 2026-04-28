dc057a397a7da73308ba6c0843b5e9c12b846883
Taranm1 <MalhiTaran182005@gmail.com>
2026-04-28 16:34:01 +0100

    created the remote procedure calls for servers communicating with eachother

--- internal/raft/rpc.go
+++ internal/raft/rpc.go
@@ -1,3 +1,27 @@
+package raft
 
+type AppendEntriesArgs struct {
+	Term         int
+	LeaderId     int
+	PrevLogIndex int
+	PrevLogTerm  int
+	Entries      []LogEntry
+	LeaderCommit int
+}
 
+type AppendEntriesReply struct {
+	Term    int
+	Success bool
+}
 
+type RequestVoteArgs struct {
+	Term         int
+	CandidateId  int
+	LastLogIndex int
+	LastLogTerm  int
+}
+
+type RequestVoteReply struct {
+	Term        int
+	VoteGranted bool
+}
